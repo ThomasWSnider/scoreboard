304ccb16d342c6ba3076f79f31aa167dfdf6086b
Thomas Snider <thomas.wayne.snider@gmail.com>
2024-06-10 15:18:08 -0600

    Buttons all working

--- app.js
+++ app.js
@@ -26,7 +26,44 @@ function displayScore() {
   let homeTeamElement = document.getElementById('homeScore')
   let awayTeamElement = document.getElementById('awayScore')
 
-  homeTeamElement.innerText = `homeScore`
-  awayTeamElement.innerText = `awayScore`
+  homeTeamElement.innerText = `0${homeScore}`
+  awayTeamElement.innerText = `0${awayScore}`
 
+  if (homeScore == 21) {
+    window.alert(`Home team wins!`);
+    gameOver()
+  }
+  else if (awayScore == 21) {
+    window.alert(`Away team wins!`);
+    gameOver()
+  }
+}
+
+function gameOver() {
+  let hob = document.getElementById(`plusOneBtnHome`)
+  let aob = document.getElementById(`plusOneBtnAway`)
+  let htb = document.getElementById(`plusThreeBtnHome`)
+  let atb = document.getElementById(`plusThreeBtnAway`)
+
+  hob.disabled = true;
+  aob.disabled = true;
+  htb.disabled = true;
+  atb.disabled = true;
+}
+
+function gameReset() {
+  let hob = document.getElementById(`plusOneBtnHome`)
+  let aob = document.getElementById(`plusOneBtnAway`)
+  let htb = document.getElementById(`plusThreeBtnHome`)
+  let atb = document.getElementById(`plusThreeBtnAway`)
+
+  hob.disabled = false;
+  aob.disabled = false;
+  htb.disabled = false;
+  atb.disabled = false;
+
+  homeScore = 0
+  awayScore = 0
+
+  displayScore()
 }
